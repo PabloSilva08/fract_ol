e012b9def8481fbc2484e467e27d6725b1a59b78
Pablo <pablomatemat@gmail.com>
2023-02-13 17:21:40 -0300

    começando validation_number

--- push/src/validation_push.c
+++ push/src/validation_push.c
@@ -6,7 +6,7 @@
 /*   By: pvieira- <pvieira-@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/12 10:09:40 by pvieira-          #+#    #+#             */
-/*   Updated: 2023/02/13 09:01:37 by pvieira-         ###   ########.fr       */
+/*   Updated: 2023/02/13 17:20:57 by pvieira-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,22 @@
 //	exit(22);
 //}
 
+static	void	validation_number(int argc, char **argv)
+{
+	unsigned int	i;
+//f (ft_atoi(argv[i]) < -2147483648 || ft_atoi(argv[i]) > 2147483647)
+	i = 1
+	while (i < argc)
+	{
+
+	}
+}
+
 static	char	*joining_arguments(int argc, char **argv)
 {
-	int		i;
-	char	*arg_new;
-	char	*temp;
+	unsigned int	i;
+	char			*arg_new;
+	char			*temp;
 
 	i = 1;
 	arg_new = ft_strdup("");
@@ -47,29 +58,16 @@ void	validation_push(int argc, char **argv)
 	char	*one_arg;
 	char	**split_arg;
 //	long	**arg_int;
-	int		i;
+//	int		i;
 
-	i = 0;
+//	i = 0;
+
+	validation_number(argc, argv);
 	one_arg = joining_arguments(argc, argv);
 	split_arg = ft_split(one_arg, ' ');
 	free(one_arg);
 
-
-
-
-
-
-
-
-
-
-
-//	
-//	while (argv[i])
-//	{
-//		arg_int[i] = atoi()
-//		i++;
-//	}
+/* ----------------Bloco de teste------------------------------------
 	while (split_arg[i] != NULL)
 	{
 		ft_printf("split_arg[%d] = %s\n",i ,split_arg[i]);
@@ -78,7 +76,7 @@ void	validation_push(int argc, char **argv)
 	i = 0;
 	ft_printf("\n\n");
 }
-	
+----------------Bloco de teste------------------------------------*/
 
 /*	while (argv[i])
 	{
