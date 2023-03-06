2765c022a6f6078c76553b018c7f5ed668bce5f8
pvieira- <pvieira-@student.42.rio>
2023-03-06 16:16:01 -0300

    Lab - acertando o 5

--- push/src/stack_sort_little_2.c
+++ push/src/stack_sort_little_2.c
@@ -6,21 +6,47 @@
 /*   By: pvieira- <pvieira-@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 14:36:20 by pvieira-          #+#    #+#             */
-/*   Updated: 2023/03/06 14:42:09 by pvieira-         ###   ########.fr       */
+/*   Updated: 2023/03/06 15:50:46 by pvieira-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push.h"
+	
+static void	search_little_index(int *index_0, int *index_1, t_stk **a)
+{
+	t_stk	*tmp;
+	int		count_tmp;
+
+	count_tmp = 0;
+	tmp = *a;
+	while (tmp->index != 0)
+	{
+		tmp = tmp->next;
+		count_tmp++;
+	}
+	*index_0 = count_tmp;
+	count_tmp = 0;
+	tmp = *a;
+	while (tmp->index != 1)
+	{
+		tmp = tmp->next;
+		count_tmp++;
+	}
+	*index_1 = count_tmp;
+//	if (*index_0 > *index_1)
+//		*index_0 = *index_0 - *index_1;
+//	else
+//		*index_1 = *index_1 - *index_0;
+}
 
 void	five_sort_1(t_stk **a, t_stk **b)
 {
-	t_stk	*tmp;
-	int		count_r;
-	int		*index_0;
-	int		*index_1;
+	int		count_index_0;
+	int		count_index_1;
 
-	count_r = 0;
-	tmp = *a;
-
-	
+	count_index_0 = 0;
+	count_index_1 = 0;
+	search_little_index(&count_index_0, &count_index_1, a);
+	ft_printf("i_0 = %d\ni_1 = %d\n", count_index_0, count_index_1);
+	ft_pa(b, a);
 }
