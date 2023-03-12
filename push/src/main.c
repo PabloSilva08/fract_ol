8bf8e7feea4a9f9c532ab73a7af709e5e10724e6
pvieira- <pvieira-@student.42.rio>
2023-03-12 09:23:11 -0300

    Lab - pre entrega

--- push/src/main.c
+++ push/src/main.c
@@ -6,7 +6,7 @@
 /*   By: pvieira- <pvieira-@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/11 13:15:02 by pvieira-          #+#    #+#             */
-/*   Updated: 2023/03/11 19:53:32 by pvieira-         ###   ########.fr       */
+/*   Updated: 2023/03/12 08:49:18 by pvieira-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,10 @@ int	main(int argc, char **argv)
 {
 	t_arg	arg;
 
-	if (argv[1][0] == '\0')
-		return (1);
 	if (argc > 1)
 	{
+		if (argv[1][0] == '\0')
+			return (1);
 		arg = validation_push(argc, argv);
 		push_swap(&arg);
 		free(arg.union_arg);
